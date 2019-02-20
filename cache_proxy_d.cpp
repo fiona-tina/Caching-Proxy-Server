a9535d954a5dde90b9d85b635df7dbc87390c139
prathikshaa-r <pratsr996@gmail.com>
2019-02-20 11:02:23 -0500

    socket opened

--- cache_proxy_d.cpp
+++ cache_proxy_d.cpp
@@ -3,15 +3,79 @@
  * February 18, 2019
  * Prathikshaa Rangarajan (pr109), Rijish Ganguly (rj???)
  */
+#define HTTP_PORT 80
+#define LISTEN_BACKLOG 1000
 
 #include <cstdio>
 #include <cstdlib>
+#include <netdb.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 // need to use boost libraries -- see how/why
+
+int open_server_socket(char *hostname, char *port) {
+  int fd;
+  int status;
+  struct addrinfo hints;
+  struct addrinfo *addrlist, *rm_it;
+
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  status = getaddrinfo(hostname, port, &hints, &addrlist);
+  // if()
+
+  for (rm_it = addrlist; rm_it != NULL; rm_it = rm_it->ai_next) {
+    fd = socket(rm_it->ai_family, rm_it->ai_socktype, rm_it->ai_protocol);
+
+    if (fd == -1) {
+      continue;
+    }
+
+    int yes = 1;
+    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+      exit(EXIT_FAILURE);
+    }
+
+    // bind
+    if (bind(fd, rm_it->ai_addr, rm_it->ai_addrlen) == 0) {
+      break;
+    }
+
+    close(fd);
+  }
+
+  if (listen(fd, LISTEN_BACKLOG) == -1) {
+    perror("ERROR");
+  }
+
+  return fd;
+}
 
 int main(void) {
 
   // server socket -- bind to port (80)?
+  char port[10] = "80";
+  int listener_fd = open_server_socket(NULL, port);
+
+  struct sockaddr_storage user_addr;
+  socklen_t user_addr_len = sizeof(user_addr);
+
+  int user_fd =
+      accept(listener_fd, (struct sockaddr *)&user_addr, &user_addr_len);
+
+  // recv the HTTP REQ
+  char buffer[512];
+  ssize_t read_id;
+
+  read_id = recv(user_fd, buffer, sizeof(buffer), 0); // MSG_WAITALL
+
+  printf("msg recvd:%s", buffer);
 
   // become a daemon
   // ref: http://www.netzmafia.de/skripten/unix/linux-daemon-howto.html
